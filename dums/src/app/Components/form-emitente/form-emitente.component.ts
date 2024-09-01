c40989dd53d7ba37ddf28f8588c396846351ec33
Wevertonbarbosa <wevertonlbs00@gmail.com>
2024-09-01 16:13:38 -0300

    Validação do formulário emitente completo, ajustar as requisições com o token

--- dums/src/app/Components/form-emitente/form-emitente.component.ts
+++ dums/src/app/Components/form-emitente/form-emitente.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PoDividerModule } from '@po-ui/ng-components';
 
 @Component({
@@ -7,5 +8,39 @@ import { PoDividerModule } from '@po-ui/ng-components';
   styleUrl: './form-emitente.component.css',
 })
 export class FormEmitenteComponent {
-  
+  public formCheck!: FormGroup;
+  public checkLoading: boolean = false;
+
+  constructor(private fb: FormBuilder) {
+    this.formCheck = this.fb.group({
+      nomeFantasia: ['', [Validators.required, Validators.minLength(2)]],
+      razaoSocial: ['', [Validators.required, Validators.minLength(2)]],
+      inscricaoEstadual: ['', [Validators.required, Validators.minLength(2)]],
+      fone: ['', [Validators.required, Validators.minLength(11)]],
+      codigoRegimeTributario: ['', [Validators.required, Validators.min(0)]],
+      cnpj: ['', [Validators.required, Validators.minLength(14)]],
+      endereco: this.fb.group({
+        cep: ['', [Validators.required, Validators.minLength(8)]],
+        logradouro: ['', [Validators.required, Validators.minLength(2)]],
+        nomeMunicipio: ['', [Validators.required, Validators.minLength(2)]],
+        numero: ['', [Validators.required, Validators.minLength(1)]],
+        numMunicipio: ['', [Validators.required, Validators.min(0)]],
+        bairro: ['', [Validators.required, Validators.minLength(2)]],
+        uf: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(2),
+            Validators.maxLength(2),
+          ],
+        ],
+        nomePais: ['', [Validators.required, Validators.minLength(2)]],
+        numPais: ['', [Validators.required, Validators.min(0)]],
+      }),
+    });
+  }
+
+  sendEmitente() {
+    console.log(this.formCheck.value);
+  }
 }
